daef19be55fdecf85b751cf25a6ee39b9cf3823c
Jungbumwoo <jungbumwoo@Naver.com>
2020-02-18 16:11:20 +0900

    0218

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { HashRouter, Route} from "react-router-dom";
-import Detail from "./routes/Detail";
+import BookDetail from "./routes/BookDetail";
 import Home from "./routes/Home";
 import Profile from "./routes/Profile";
+import AddBook from "./routes/AddBook";
 
 
 function App() {
   return (
     <HashRouter>
       <Route path="/" exact={true} component={Home}/>
-      <Route path="/:id" component={Detail}/>
+      <Route path="/addbook" component={AddBook} />
+      <Route path="/book/:id" component={BookDetail}/>
       <Route path="/profile" component={Profile}/>
     </HashRouter>
   );
